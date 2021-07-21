bfcf87c2dc8c3f3b1f59ec6458dfb203fa7984ed
Administrator <admin@example.com>
2021-07-21 23:33:13 +0200

    Remove bootstrap.sql from repository

--- bootstrap.sql
+++
@@ -1,5 +0,0 @@
-/* TABLE NEED */
-CREATE TABLE need (id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(100), priority VARCHAR(100)) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-INSERT INTO need (name, priority) VALUES ('partage', 'haut');
-INSERT INTO need (name, priority) VALUES ('accomplissement personnel', 'bas');
